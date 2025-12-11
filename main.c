8ca27f66cd2b12e1762cbac20a073ed2b8408a43
psauhardata04 <lamapsauhardata@gmail.com>
2025-12-11 20:10:54 +0545

    lanes.h and lanes.c added

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include "queue.h"
 #include "traffic_light.h"
+#include "lanes.h"
 
 int main() {
     LightState light = RED;
+    LaneSystem lanes;
+
+    initLanes(&lanes);
 
     printf("Traffic Light Simulator Started\n");
 
+    // Demo: add some vehicles
+    enqueue(&lanes.A, 101);
+    enqueue(&lanes.B, 202);
+
+    printf("Front of lane A: %d\n", peek(&lanes.A));
+    printf("Front of lane B: %d\n", peek(&lanes.B));
+
+    // Cycle lights
     for (int i = 0; i < 5; i++) {
         printf("Current Light: %d\n", light);
         changeLight(&light);
