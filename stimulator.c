5d6dbb5a905f0c56f12a139c163fb27aa31a875e
psauhardata04 <lamapsauhardata@gmail.com>
2025-12-25 22:09:59 +0545

    updated and deleted extra files

--- stimulator.c
+++
@@ -1,7 +0,0 @@
-
-#include <stdio.h>
-
-int main() {
-    printf("Traffic Simulator Initialized\n");
-    return 0;
-}
--- traffic_generator.c
+++
@@ -1,7 +0,0 @@
-
-#include <stdio.h>
-
-int main() {
-    printf("Traffic Generator Started (Day 1)\n");
-    return 0;
-}
